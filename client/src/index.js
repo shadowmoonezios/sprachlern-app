34dadbeff63b879d249677eacace4855ab389a64
shadowmoonezios <agushifelde@hotmail.com>
2025-09-28 15:49:24 +0300

    feat: initial commit for client/src/index.js

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './App';
+
+const store = createStore(() => ({})); // Einfache Platzhalter-Reduzierung
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
